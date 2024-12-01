b1cfc82e913bb40e7ce3e32cb3bf691e8ffc4437
m-triple-m <triplem656@gmail.com>
2024-12-01 15:04:19 +0530

    added example

--- object_exercise.js
+++ object_exercise.js
@@ -1,23 +1,37 @@
 const clothList = [
     {
-        brand : 'Zudio',
-        name : 'T-shirt',
-        price : 500,
-        size : ['M', 'L', 'XL'],
-        color : ['black', 'blue', 'red']
+        brand: 'Zudio',
+        name: 'T-shirt',
+        price: 500,
+        size: ['M', 'L', 'XL'],
+        color: ['black', 'blue', 'red']
     },
     {
-        brand : 'Levis',
-        name : 'Jeans',
-        price : 1500,
-        size : ['S', 'M', 'L', 'XL', 'XXL'],
-        color : ['black', 'blue', 'grey']
+        brand: 'Levis',
+        name: 'Jeans',
+        price: 1500,
+        size: ['S', 'M', 'L', 'XL', 'XXL'],
+        color: ['black', 'blue', 'grey']
     },
     {
-        brand : 'Puma',
-        name : 'Shorts',
-        price : 800,
-        size : ['M', 'L'],
-        color : ['black', 'blue', 'red']
+        brand: 'Puma',
+        name: 'Shorts',
+        price: 800,
+        size: ['M', 'L'],
+        color: ['black', 'blue', 'red']
+    },
+    {
+        brand: 'Adidas',
+        name: 'Track Pant',
+        price: 1200,
+        size: ['M', 'L', 'XL'],
+        color: ['black', 'blue', 'grey']
+    },
+    {
+        brand: 'Reebok',
+        name: 'Capri',
+        price: 1000,
+        size: ['S', 'M', 'L'],
+        color: ['black', 'blue', 'red']
     }
 ]
